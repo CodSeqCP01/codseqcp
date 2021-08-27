3b3eb18c4b74e4e4e9d283c35f68a78131045158
CodSeqCP01 <61832232+CodSeqCP01@users.noreply.github.com>
2021-08-27 10:22:08 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='codseqcp',
+      version = '0.0.0',
+      description = 'Pipeline for Clustering Homologous Coding or Genomic Sequence from Raw Fastq Reads',
+      url = 'https://github.com/CodSeqCP01',
+      author = 'Edson Ishengoma',
+      author_email = 'edson.ishengoma@muce.ac.tz',
+      license = 'MIT',
+      packages = ['codseqcp']
+      )
